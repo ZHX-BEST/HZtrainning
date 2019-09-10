77b5add2feb986a1e756a5fd9d59ae85de3cdfdb
PiscesBig <593401232@qq.com>
2019-09-10 18:11:06 +0800

    头头修改

---
+++ HangzhouPeiXun/HangzhouPeiXun/DAL/Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HangzhouPeiXun.DAL
+{
+    public class Test
+    {
+        private Test mytest = new Test();
+        public Test MyTest { get { return mytest; } }
+        public Test() { }
+    }
+}
