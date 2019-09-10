5e164ecc49124f856e8a9515be00bc2bc92c9820
PiscesBig <593401232@qq.com>
2019-09-10 17:55:08 +0800

    头头修改

--- HangzhouPeiXun/HangzhouPeiXun/DAL/Login.cs
+++ HangzhouPeiXun/HangzhouPeiXun/DAL/Login.cs
@@ -9,10 +9,34 @@ namespace HangzhouPeiXun.DAL
 {
     public class Login
     {
+        private Login mylogin = new Login();
+        public Login MyLogin { get { return mylogin; } }
+        public Login() { }
+
+        /// <summary>
+        /// 学员登录
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <param name="pwd"></param>
+        /// <returns></returns>
         public DataTable getlogin(string ID,string pwd)
         {
-            string sql = "select * from TB_User where User_ID = @ID and User_PWD = @pwd";
+            string sql = "select * from TB_User where User_ID = @ID and User_PWD = @pwd and User_Teacher =0";
             SqlParameter[] paras = {new SqlParameter("@ID",ID),new SqlParameter("@pwd",pwd) }; 
+            DataTable dt = new Helper.SQLHelper().ExcuteQuery(sql, paras, CommandType.Text);
+            return dt;
+        }
+
+        /// <summary>
+        /// 教师登录
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <param name="pwd"></param>
+        /// <returns></returns>
+        public DataTable getteacherlogin(string ID, string pwd)
+        {
+            string sql = "select * from TB_User where User_ID = @ID and User_PWD = @pwd and User_Teacher =1";
+            SqlParameter[] paras = { new SqlParameter("@ID", ID), new SqlParameter("@pwd", pwd) };
             DataTable dt = new Helper.SQLHelper().ExcuteQuery(sql, paras, CommandType.Text);
             return dt;
         }
