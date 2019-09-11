486bfd75efa18a57ea6e8e8bfc3d8c598940a52d
PiscesBig <593401232@qq.com>
2019-09-11 16:30:16 +0800

    头头修改9.11

---
+++ HangzhouPeiXun/HangzhouPeiXun/DAL/Examples.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HangzhouPeiXun.DAL
+{
+    public class Examples
+    {
+        private Examples myexamples = new Examples();
+        public Examples MyExamples { get { return myexamples; } }
+        public Examples() { }
+    }
+}
