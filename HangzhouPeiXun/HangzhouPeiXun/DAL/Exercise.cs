ba99fd334757e35032357f151698dd59d141df55
PiscesBig <593401232@qq.com>
2019-09-30 10:44:05 +0800

    头头

--- HangzhouPeiXun/HangzhouPeiXun/DAL/Exercise.cs
+++ HangzhouPeiXun/HangzhouPeiXun/DAL/Exercise.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace HangzhouPeiXun.DAL
 {
@@ -13,6 +15,26 @@ namespace HangzhouPeiXun.DAL
         private static Exercise myexercise = new Exercise();
         public static Exercise MyExercise { get { return myexercise; } }
         public Exercise() { }
+
+        #region 获取正常数据
+        public DataTable getNormalData(string NorID, string option)
+        {
+            SqlParameter[] paras = new SqlParameter[] { new SqlParameter("@NorID", NorID) };
+            string sql = "select * from TB_" + option + " where " + option + "_DataID = @NorID";
+            DataTable dt = new Helper.SQLHelper().ExcuteQuery(sql, paras, CommandType.Text);
+            return dt;
+        }
+        #endregion
+
+        #region 获取异常数据
+        public DataTable getAbnormalData(string AbID, string option)
+        {
+            SqlParameter[] paras = new SqlParameter[] { new SqlParameter("@AbID", AbID) };
+            string sql = "select * from TB_" + option + " where " + option + "_DataID = @AbID";
+            DataTable dt = new Helper.SQLHelper().ExcuteQuery(sql, paras, CommandType.Text);
+            return dt;
+        }
+        #endregion
 
     }
 }
